9c07adbd347a1758705b6f9e83959a9403766bf1
Tikhon Ishchenko <tikhon.ishchenko@ukma.edu.ua>
2023-11-03 01:18:28 +0200

    added authorization for 2 roles

--- src/main/java/com/cyber_chill/configuration/WebSecurityConfig.java
+++ src/main/java/com/cyber_chill/configuration/WebSecurityConfig.java
@@ -1,20 +1,47 @@
 package com.cyber_chill.configuration;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
+
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
 
     @Bean
-    public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+    public UserDetailsService userDetailsService() throws Exception {
+        // ensure the passwords are encoded properly
+        User.UserBuilder users = User.withDefaultPasswordEncoder();
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        manager.createUser(users.username("admin").password("adminpass").roles("ADMIN").build());
+        manager.createUser(users.username("user").password("userpass").roles("USER").build());
+        return manager;
+    }
 
+    @Bean
+    public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((requests) -> requests
+                        .requestMatchers(antMatcher("/*")).permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin((form) -> form
+                        .loginPage("/login")
+                        .defaultSuccessUrl("/login/loginsuccessfull", true)
+                        .permitAll()
+                )
+                .logout((logout) -> logout.permitAll());
         return http.build();
     }
 
